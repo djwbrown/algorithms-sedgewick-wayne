9a7aa7680fae535af7fbcf8285042e8d48c57b48
Dylan Brown <djwbrown@gmail.com>
2017-02-13 23:02:59 -0800

    lifo-stack-resizing-array works.

--- src/lifo-stack-resizing-array.cpp
+++ src/lifo-stack-resizing-array.cpp
@@ -10,43 +10,86 @@ using namespace std;
 
 template<typename T> class ResizingArrayStack {
 public:
+    // Self-reflective functions on the status of the stack.
     bool is_empty() { return ( N == 0 ); }
     int size() { return N; }
     
     void push(T item);
+    T pop();
+
+    T* begin() { return &array_ptr[0]; }
+    T* end() { return &array_ptr[N]; }
+
+    ~ResizingArrayStack() {
+        delete [] array_ptr;
+    }
+
 private:
+    // Allocate space for a traditional array on the heap.
+    T* array_ptr = new T[1];
+    // Keep track of the space allocated for the array, max_size * sizeof(T).
     int max_size = 1;
+    // Keep track of the current number of items on the stack.
     int N = 0;
-    T* a = new T[1];
 
     void resize(int new_size);
 };
 
 template<typename T> void ResizingArrayStack<T>::push(T item)
 {
-    if (++N == max_size) { resize(2 * max_size); }
-    a[N] = item;
+    if (N == max_size) { resize(2 * max_size); }
+    // Push to the stack and increment the current count of items.
+    array_ptr[N++] = item;
+}
+
+template<typename T> T ResizingArrayStack<T>::pop()
+{
+    // Remember that max index is N-1, so prefix decrement to pop from the stack.
+    T item = array_ptr[--N];
+    // Shrink the array if needed.
+    if ( N > 0 && N == (max_size / 4) ) { resize(max_size / 2); }
+    return item;
 }
 
 template<typename T> void ResizingArrayStack<T>::resize(int new_size)
 {
     max_size = new_size;
-    T* temp = new T[max_size];
+    // Allocate a replacement array with space adequate for the stack.
+    T* replacement = new T[max_size];
+    // Copy each element into the replacement array.
     for (int i = 0; i < N; i++) {
-        temp[i] = a[i];
+        replacement[i] = array_ptr[i];
     }
-    delete [] a;
-    a = temp;
+    // Free the memory associated with the old array.
+    delete [] array_ptr;
+    // Reset the pointer to the newly resized array.
+    array_ptr = replacement;
 }
 
 int main ()
 {
+    // Create the stack with type `double`.
     auto lifo_stack = ResizingArrayStack<double>{};
-    cout << "Created an empty LIFO stack for `double`s, with a resizing array implementation." << endl;
-
-    lifo_stack.push(3.14);
-    lifo_stack.push(0.99);
+    cout << "Created an empty LIFO stack for double type items, with a resizing array implementation." << endl;
+    
+    // Fill the stack
     lifo_stack.push(1.01);
+    lifo_stack.push(2.02);
+    lifo_stack.push(3.14);
+    lifo_stack.push(4.04);
+    cout << "Request status, the LIFO stack has size: " << lifo_stack.size() << endl;
+    cout << "Request status, the LIFO stack is empty: " << (lifo_stack.is_empty() ? "true" : "false") << endl;
+
+    // Here we're iterating forward through the array, with an unused variable `i`.
+    // It would be nice performance-wise to iterate in reverse without calling pop(), and without triggering a resize.
+    for ( auto& i : lifo_stack ) {
+        cout << "Popped an item from the stack: " << lifo_stack.pop() << endl;
+    }
+    // cout << "Popped an item from the stack: " << lifo_stack.pop() << endl;
+    // cout << "Popped an item from the stack: " << lifo_stack.pop() << endl;
+    // cout << "Popped an item from the stack: " << lifo_stack.pop() << endl;
+    // cout << "Popped an item from the stack: " << lifo_stack.pop() << endl;
+    
     cout << "Request status, the LIFO stack has size: " << lifo_stack.size() << endl;
     cout << "Request status, the LIFO stack is empty: " << (lifo_stack.is_empty() ? "true" : "false") << endl;
 }
